3db90727ef4fa6d78bb74a9ce019de3f1663638a
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-03-13 03:46:18 +0200

    add railway cred

--- backend/api/db.php
+++ backend/api/db.php
@@ -1,11 +1,17 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "grademe";
+// $host = "localhost";
+// $user = "root";
+// $password = "";
+// $dbname = "grademe";
 
-$conn = new mysqli($host, $user, $password, $dbname);
+$host = "DB_HOST";
+$user = "DB_USER";
+$password = "DB_PASS";
+$dbname = "DB_NAME";
+
+$conn = new mysqli($host, $user, $password, $dbname, 59142);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
